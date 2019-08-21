9081e664c1a8b41e1aefb101aa63588e36a3e663
zhangshaoyan <1811543668@qq.com>
2019-08-21 16:43:39 +0800

    add amixer audio device cset/cget code.

--- p16_no_gui/alsa/zaudiotask.h
+++ p16_no_gui/alsa/zaudiotask.h
@@ -7,6 +7,62 @@
 #include "zaudiotxthread.h"
 #include <QTimer>
 
+/**
+ *
+zhangshaoyan@YNTP16:~/p16$ aplay -l
+List of PLAYBACK Hardware Devices ****
+card 0: tegrahda [tegra-hda], device 3: HDMI 0 [HDMI 0]
+  Subdevices: 1/1
+  Subdevice #0: subdevice #0
+card 0: tegrahda [tegra-hda], device 7: HDMI 0 [HDMI 0]
+  Subdevices: 1/1
+  Subdevice #0: subdevice #0
+card 1: Device [USB Audio Device], device 0: USB Audio [USB Audio]
+  Subdevices: 0/1
+  Subdevice #0: subdevice #0
+zhangshaoyan@YNTP16:~/p16$
+
+zhangshaoyan@YNTP16:~/p16$ amixer -c 1 controls
+numid=3,iface=MIXER,name='Mic Playback Switch'
+numid=4,iface=MIXER,name='Mic Playback Volume'
+numid=7,iface=MIXER,name='Mic Capture Switch'
+numid=8,iface=MIXER,name='Mic Capture Volume'
+numid=9,iface=MIXER,name='Auto Gain Control'
+numid=5,iface=MIXER,name='Speaker Playback Switch'
+numid=6,iface=MIXER,name='Speaker Playback Volume'
+numid=2,iface=PCM,name='Capture Channel Map'
+numid=1,iface=PCM,name='Playback Channel Map'
+zhangshaoyan@YNTP16:~/p16$
+
+
+
+zhangshaoyan@YNTP16:~/p16$ amixer -c 1 cset numid=6,iface=MIXER,name='Speaker Playback Volume' 10
+numid=6,iface=MIXER,name='Speaker Playback Volume'
+  ; type=INTEGER,access=rw---R--,values=2,min=0,max=30,step=0
+  : values=10,10
+  | dBminmax-min=-45.00dB,max=0.00dB
+
+zhangshaoyan@YNTP16:~/p16$ amixer -c 1 cget numid=8,iface=MIXER,name='Mic Capture Volume'
+numid=8,iface=MIXER,name='Mic Capture Volume'
+  ; type=INTEGER,access=rw---R--,values=1,min=0,max=30,step=0
+  : values=12
+  | dBminmax-min=-12.00dB,max=33.00dB
+zhangshaoyan@YNTP16:~/p16$
+
+
+zhangshaoyan@YNTP16:~/p16$ amixer -c 1 cset numid=8,iface=MIXER,name='Mic Capture Volume' 20
+numid=8,iface=MIXER,name='Mic Capture Volume'
+  ; type=INTEGER,access=rw---R--,values=1,min=0,max=30,step=0
+  : values=20
+  | dBminmax-min=-12.00dB,max=33.00dB
+
+zhangshaoyan@YNTP16:~/p16$ amixer -c 1 cset numid=9,iface=MIXER,name='Auto Gain Control' 1
+numid=9,iface=MIXER,name='Auto Gain Control'
+  ; type=BOOLEAN,access=rw------,values=1
+  : values=on
+zhangshaoyan@YNTP16:~/p16$
+ *
+ */
 class ZAudioTask : public QObject
 {
     Q_OBJECT
